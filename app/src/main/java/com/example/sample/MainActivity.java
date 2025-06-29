b5f7ea239220dc3193ce9c9e7f11c342346fda0a
sho09214 <motohashi.sho@gmail.com>
2025-06-29 22:03:53 +0900

    Add - 足し算機能 追加

--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sample;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onButton(View view){
+        //Editの取得
+        EditText eT1 = (EditText) findViewById(R.id.eT1);
+        EditText eT2 = (EditText) findViewById(R.id.eT2);
+
+        //Edit→String
+        String str1 = eT1.getText().toString();
+        String str2 = eT2.getText().toString();
+
+        //数値に変換
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+
+        //足し算
+        int sum = num1 + num2;
+
+        ((TextView)findViewById(R.id.tvAns)).setText(String.valueOf(sum));
+    }
+
+    public void onClear(View view){
+        ((EditText)findViewById(R.id.eT1)).getText().clear();
+        ((EditText)findViewById(R.id.eT2)).getText().clear();
+        ((TextView)findViewById(R.id.tvAns)).setText("Answer");
     }
 }
